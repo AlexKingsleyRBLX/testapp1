d8127f54ae53883b8383ba9f2d6c24e3f412fe27
AlexKingsleyRBLX <33580756+AlexKingsleyRBLX@users.noreply.github.com>
2019-12-03 11:21:12 -0500

    Update and rename Variables to Variables.tf

---
+++ Variables.tf
@@ -0,0 +1,15 @@
+variable "User" {
+  	description = "Current Active User"
+  	default = "cc1-2x4"
+}
+
+variable "Permissions" {
+  	description = "Permissions the Current Active User has"
+  	default = "cc1-2x5"
+}
+
+
+variable "CommandConsole" {
+  	description = "Server Command Console Accessable by the user with permissions of 3+"
+  	default = "cc1-2x6"
+}
